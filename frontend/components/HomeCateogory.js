a286eec2fb344ccdf8cc750d47bc639e8ec7e709
Arun Gawri <luckygawri@gmail.com>
2022-04-05 02:11:19 +0530

    Home Layout Done

--- frontend/components/HomeCateogory.js
+++ frontend/components/HomeCateogory.js
@@ -3,19 +3,21 @@ import Image from 'next/image'
 
 const HomeCateogory = ({ category, index }) => {
 	return (
-		<div className={`flex ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"}`}>
+		<div className="container mx-auto border-b pb-8">
+			<div className={`flex ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"} items-center justify-around`}>
 			<Image src={category?.imageUrl.replace("/static", "") ?? ""} width={300} height={180} />
-			<div className="">
+				<div className="flex flex-col items-center gap-3">
 				<div className="">
 					{category.name}
 				</div>
 				<div className="">
 					{category.description}
 				</div>
-				<button className="text-white bg-pink-800 p-1 font-thin">
+					<button className="text-white bg-pink-800 p-2 font-extralight">
 					Explore {category.key}
 				</button>
 			</div>
+		</div>
 		</div>
 	)
 }
