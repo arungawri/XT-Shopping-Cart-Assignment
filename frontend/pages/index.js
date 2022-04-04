b4a3b5e26153509cf843b6dbdeb3b6c1336c1d39
Arun Gawri <luckygawri@gmail.com>
2022-04-04 19:47:20 +0530

    Carousel Install

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from "next/image"
 import { useState, useEffect } from 'react'
 import HomeCateogory from '../components/HomeCateogory'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Home() {
   const [data, setData] = useState(null)
@@ -32,11 +34,13 @@ export default function Home() {
       <div className="text-xl">
         Carousel
       </div>
-      <Image src={"/images/offers/offer1.jpg"} width={1000} height={300} />
-      {/* <Image src={"/images/offers/offer2.jpg"} width={1000} height={300} />
-      <Image src={"/images/offers/offer3.jpg"} width={1000} height={300} />
-      <Image src={"/images/offers/offer4.jpg"} width={1000} height={300} />
-      <Image src={"/images/offers/offer5.jpg"} width={1000} height={300} /> */}
+      <Carousel showArrows>
+        <Image src={"/images/offers/offer1.jpg"} width={1000} height={300} />
+        <Image src={"/images/offers/offer2.jpg"} width={1000} height={300} />
+        <Image src={"/images/offers/offer3.jpg"} width={1000} height={300} />
+        <Image src={"/images/offers/offer4.jpg"} width={1000} height={300} />
+        <Image src={"/images/offers/offer5.jpg"} width={1000} height={300} />
+      </Carousel>
       <h1 className="text-xl">
         Grid/Component Alternate left right
       </h1>
