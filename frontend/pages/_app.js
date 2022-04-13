ff08f0b9330ff618cdecb6ec3e6389aec10bc671
Arun Gawri <luckygawri@gmail.com>
2022-04-13 12:11:58 +0530

    Add cart component to _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,15 +1,20 @@
+import { useState } from 'react'
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '../store';
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Cart from '../components/Cart'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const [showCartModal, setShowCartModal] = useState(false)
+
   return (
     <>
       <ReduxProvider store={store}>
         <Header />
         <Component {...pageProps} />
+        {showCartModal && <Cart />}
         <Footer />
       </ReduxProvider>
     </>
